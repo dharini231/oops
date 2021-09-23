245709aec232b010ed0dd484a67911ad50c41a3a
dharini231 <90209992+dharini231@users.noreply.github.com>
2021-09-24 02:53:40 +0800

    Add files via upload

---
+++ AC.java
@@ -0,0 +1,21 @@
+package com.xworks.oops;
+
+public class AC {
+	String color;
+	int price;
+	int capacity;
+	String model;
+	public void cool() {
+	System.out.println("cooling");
+	}
+	public void switchon() {
+		System.out.println("ac switchon");
+		}
+	public void switchoff() {
+		System.out.println("ac switchoff");
+		}
+	
+        
+	
+
+}
